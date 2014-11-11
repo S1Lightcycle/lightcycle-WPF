3919336cbf8c2e3f7cec6fe43b50e5124f80b0d5
Nepetalactone <tobias.meusburger@gmx.net>
2014-11-11 19:45:04 +0100

    Saving option settings
    Signed-off-by: Nepetalactone <tobias.meusburger@gmx.net>

--- S1lightcycle/S1lightcycle/OptionWindow.xaml.cs
+++ S1lightcycle/S1lightcycle/OptionWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,29 @@ namespace S1lightcycle {
             this.LearningRate.Text = Properties.Settings.Default.LearningRate.ToString();
             this.MinBlobSize.Text = Properties.Settings.Default.MinBlobSize.ToString();
             this.MaxBlobsize.Text = Properties.Settings.Default.MaxBlobSize.ToString();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            double learningRate = 0;
+            if ((LearningRate.Text != "") && (Double.TryParse(LearningRate.Text, out learningRate)))
+            {
+                Properties.Settings.Default.LearningRate = learningRate;
+            }
+
+            int minBlobSize = 0;
+            if ((MinBlobSize.Text != "") && (Int32.TryParse(MinBlobSize.Text, out minBlobSize)))
+            {
+                Properties.Settings.Default.MinBlobSize = minBlobSize;
+            }
+
+            int maxBlobSize = 0;
+            if ((MaxBlobsize.Text != "") && (Int32.TryParse(MaxBlobsize.Text, out maxBlobSize)))
+            {
+                Properties.Settings.Default.MaxBlobSize = maxBlobSize;
+            }
+            Properties.Settings.Default.Save();
+            base.OnClosing(e);
         }
     }
 }
