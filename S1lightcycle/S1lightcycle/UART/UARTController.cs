66032e348b91bb48495de0c3df6f433a9df30d52
Nepetalactone <tobias.meusburger@gmx.net>
2014-12-04 14:03:38 +0100

    init UART

---
+++ S1lightcycle/S1lightcycle/UART/UARTController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace S1lightcycle.UART
+{
+    class UARTController
+    {
+        private SerialPort _comPort;
+
+        private static UARTController _instance;
+        public UARTController Instance 
+        { 
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new UARTController();
+                }
+                return _instance;
+            } 
+        }
+
+        private UARTController()
+        {
+            _comPort = new SerialPort();
+        }
+
+    }
+}
