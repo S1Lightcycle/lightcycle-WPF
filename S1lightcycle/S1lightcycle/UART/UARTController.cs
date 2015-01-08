ab9c5a71b594a5aaf829dcdc3a22d41f8fef86d3
sme3863 <a.a@a.a>
2015-01-08 13:31:17 +0100

    asdf

--- S1lightcycle/S1lightcycle/UART/UARTController.cs
+++ S1lightcycle/S1lightcycle/UART/UARTController.cs
@@ -9,10 +9,19 @@ namespace S1lightcycle.UART
 {
     class UARTController
     {
+        enum Command{
+            Heartbeat, Forward, Reverse, RightStatic, LeftStatic, Right, Left, SetSpeed
+        }
+        enum Receiver
+        {
+            Server = 0, Client1 = 1, Client2 = 2, Broadcast = 7
+        }
+
+
         private SerialPort _comPort;
 
         private const String DefaultPortName = "COM4";
-        private const int DefaultBaudRate = 9600;
+        private const int DefaultBaudRate = 115200;
         private const int DefaultDataBits = 8;
         private const Parity DefaultParity = Parity.None;
         private const StopBits DefaultStopBits = StopBits.One;
@@ -44,12 +53,32 @@ namespace S1lightcycle.UART
             };
         }
 
-        private void Write(String message)
+
+        public void Write(Receiver receiver, Command command, int parameter)
         {
-            _comPort.Open();
-            _comPort.Write(message);
-            _comPort.Close();
+            try
+            {
+                _comPort.Open();
+                _comPort.Write("" + receiver + command + parameter);
+            }
+            finally
+            {
+                _comPort.Close();
+            }
         }
 
+        public void Write(String message)
+        {
+            try
+            {
+                _comPort.Open();
+                _comPort.Write(message);
+            }
+            finally
+            {
+                _comPort.Close();
+            }
+            
+        }
     }
 }
